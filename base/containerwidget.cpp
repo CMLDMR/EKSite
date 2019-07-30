60da785d55607d39e555e2aad7dc7156fc07c8fc
Cemal Demir <CMLDMR@users.noreply.github.com>
2019-07-30 11:02:03 +0300

    setContainer Bootstrap ContainerType.

--- base/containerwidget.cpp
+++ base/containerwidget.cpp
@@ -4,3 +4,21 @@ ContainerWidget::ContainerWidget()
 {
 
 }
+
+void ContainerWidget::setContainerType(ContainerWidget::ContainerType type)
+{
+    switch (type) {
+
+    case ContainerType::ROW:
+        addStyleClass(Bootstrap::Grid::row);
+        break;
+
+    case ContainerType::CONTAINERFLUID:
+        addStyleClass(Bootstrap::Grid::container_fluid);
+        break;
+    default:
+
+        break;
+
+    }
+}
--- base/containerwidget.h
+++ base/containerwidget.h
@@ -1,11 +1,21 @@
 #ifndef CONTAINERWIDGET_H
 #define CONTAINERWIDGET_H
 
+#include "wtinclude.h"
+#include "bootstrap.h"
+#include "inlinestyle.h"
 
 class ContainerWidget : public WContainerWidget
 {
 public:
     ContainerWidget();
+
+    enum class ContainerType{
+        ROW,
+        CONTAINERFLUID
+    };
+
+    void setContainerType(ContainerType type);
 };
 
 #endif // CONTAINERWIDGET_H
