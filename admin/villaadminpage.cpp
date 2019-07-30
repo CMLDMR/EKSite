6376751aef138bc467c2d3c95940703d6ce45280
Cemal Demir <CMLDMR@users.noreply.github.com>
2019-07-30 11:23:47 +0300

    VillaAdminPage added.

---
+++ admin/villaadminpage.cpp
@@ -0,0 +1,6 @@
+#include "villaadminpage.h"
+
+VillaAdminPage::VillaAdminPage()
+{
+
+}
---
+++ admin/villaadminpage.h
@@ -0,0 +1,11 @@
+#ifndef VILLAADMINPAGE_H
+#define VILLAADMINPAGE_H
+
+
+class VillaAdminPage : public ContainerWidget
+{
+public:
+    VillaAdminPage();
+};
+
+#endif // VILLAADMINPAGE_H
