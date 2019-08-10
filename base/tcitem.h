4370d520f0db70965d426a066b50197db443d412
Cemal Demir <CMLDMR@users.noreply.github.com>
2019-08-10 19:43:15 +0300

    Kısmı Tamamlandı.

--- base/tcitem.h
+++ base/tcitem.h
@@ -1,11 +1,41 @@
 #ifndef TCITEM_H
 #define TCITEM_H
 
+#include "itembase.h"
 
-class TCItem : public DBClass
+namespace TC {
+
+namespace KEY {
+static const std::string collection{"tcCollection"};
+static const std::string tcno{"tcno"};
+static const std::string telefon{"telefon"};
+static const std::string adsoyad{"adsoyad"};
+static const std::string adress{"adres"};
+static const std::string email{"email"};
+}
+
+class TCItem : public ItemBase
 {
 public:
-    TCItem();
+    static boost::optional<TCItem> Create_TC(mongocxx::database* _db,
+                                      const std::string &tcno,
+                                      const std::string &telefon,
+                                      const std::string &adsoyad,
+                                      const std::string &adres,
+                                      const std::string &email);
+
+    static QVector<TCItem*> GetList(mongocxx::database* _db , const bsoncxx::document::view &filter );
+
+
+private:
+    TCItem(mongocxx::database* _db);
+    TCItem(mongocxx::database* _db , bsoncxx::document::value value );
+
+
 };
+
+
+}
+
 
 #endif // TCITEM_H
