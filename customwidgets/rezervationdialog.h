fb2d12989dd1ed673e77213a6b3a1ab21fc19400
Cemal Demir <CMLDMR@users.noreply.github.com>
2019-08-10 13:55:17 +0300

    RezervationDialog Tamamlandı.

--- customwidgets/rezervationdialog.h
+++ customwidgets/rezervationdialog.h
@@ -1,11 +1,39 @@
 #ifndef REZERVATIONDIALOG_H
 #define REZERVATIONDIALOG_H
 
+#include "base/dbclass.h"
+#include "base/containerwidget.h"
 
-class RezervationDialog : public WDialog
+class RezervationDialog : public WDialog , public DBClass
 {
 public:
-    RezervationDialog();
+    RezervationDialog(mongocxx::database* _db);
+
+
+    std::int64_t beginJulianDate() const;
+    void setBeginJulianDate(const std::int64_t &beginJulianDate);
+
+    std::int64_t endJulianDate() const;
+    void setEndJulianDate(const std::int64_t &endJulianDate);
+
+    int kisiSayisi() const;
+    void setKisiSayisi(int kisiSayisi);
+
+private:
+    std::int64_t mBeginJulianDate;
+    std::int64_t mEndJulianDate;
+    int mKisiSayisi;
+
+    WText* mBeginJulianDateText;
+    WText* mEndJulianDateText;
+    WText* mTotalJulianDateText;
+    WLineEdit* mTelefonNumarasiLineEdit;
+    WLineEdit* mAdSoyadLineEdit;
+    WLineEdit* mEmailLineEdit;
+    WText* mKisiSayisiText;
+
+    void Save();
+
 };
 
 #endif // REZERVATIONDIALOG_H
