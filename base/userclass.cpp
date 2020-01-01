8f6163d89d398fcefc005ffc4076befefe160a74
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-01 13:18:15 +0300

    Deleted.

--- base/userclass.cpp
+++
@@ -1,6 +0,0 @@
-#include "userclass.h"
-
-UserClass::UserClass()
-{
-
-}
--- newClass/rezervationlist.cpp
+++
@@ -1,6 +0,0 @@
-#include "rezervationlist.h"
-
-
-void Rezervation::RezervationList::onList(const QVector<eCore::RezervationItem> &mlist)
-{
-}
