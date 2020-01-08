e9439ebaa7e0f7fab397b39f859a29d7ac93fb65
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-08 19:51:31 +0300

    VillaItem Tamamlandı.

--- villa/villa.cpp
+++ villa/villa.cpp
@@ -55,6 +55,39 @@ VillaItem &VillaItem::setTanim(const std::string &tanim)
     return *this;
 }
 
+VillaItem &VillaItem::setKisiSayisi(const int &kisiSayisi)
+{
+    this->append(villaKey::kisiSayi,bsoncxx::types::b_int32{kisiSayisi});
+    return *this;
+}
+
+VillaItem &VillaItem::setHavuz(const std::string &havuz)
+{
+    this->append(villaKey::havuz,havuz);
+    return *this;
+}
+
+VillaItem &VillaItem::setFotoList(const QVector<bsoncxx::oid> list)
+{
+    for( auto itemOid : list )
+    {
+        this->pushArray(villaKey::fotoList,itemOid);
+    }
+    return *this;
+}
+
+VillaItem &VillaItem::addFotoList(const bsoncxx::oid &fotoOid)
+{
+    this->pushArray(villaKey::fotoList,fotoOid);
+    return *this;
+}
+
+VillaItem &VillaItem::setYayinda(const bool &yayinda)
+{
+    this->append(villaKey::yayinda,yayinda);
+    return *this;
+}
+
 const std::string VillaItem::adi() const
 {
     auto val = this->element (villaKey::adi);
@@ -93,6 +126,51 @@ const std::string VillaItem::tanim() const
         return val.value ().get_utf8 ().value.to_string();
     }
     return "";
+}
+
+const std::string VillaItem::havuz() const
+{
+    auto val = this->element (villaKey::havuz);
+    if( val )
+    {
+        return val.value ().get_utf8 ().value.to_string();
+    }
+    return "";
+}
+
+const bool VillaItem::yayinda() const
+{
+    auto val = this->element (villaKey::yayinda);
+    if( val )
+    {
+        return val.value ().get_bool ().value;
+    }
+    return false;
+}
+
+const QVector<bsoncxx::oid> VillaItem::fotoList() const
+{
+    QVector<bsoncxx::oid> list;
+    auto val = this->element (villaKey::fotoList);
+    if( val )
+    {
+        auto array = val.value ().get_array ().value;
+        for( auto item : array )
+        {
+            list.push_back (item.get_oid ().value);
+        }
+    }
+    return list;
+}
+
+ int VillaItem::kisiSayisi() const
+{
+    auto val = this->element (villaKey::kisiSayi);
+    if( val )
+    {
+        return val.value ().get_int32 ().value;
+    }
+    return 0;
 }
 
 void VillaItem::errorOccured(const std::string &errorText)
--- villa/villa.h
+++ villa/villa.h
@@ -12,12 +12,16 @@ static const std::string adi{"adi"};
 static const std::string yeri{"yeri"};
 static const std::string fotoOid{"fotoOid"};
 static const std::string tanim{"tanim"};
+static const std::string kisiSayi{"kisiSayisi"};
+static const std::string havuz{"havuz"};
+static const std::string fotoList{"fotoList"};
+static const std::string yayinda{"yayinda"};
 }
 
 class VillaItem : public eCore::Item
 {
 public:
-    VillaItem();
+    VillaItem( );
     VillaItem( const VillaItem& other );
     VillaItem( VillaItem&& other );
 
@@ -28,11 +32,23 @@ public:
     VillaItem& setYeri( const std::string& yeri );
     VillaItem& setFotoOid( const std::string& fotoOid );
     VillaItem& setTanim( const std::string& tanim );
+    VillaItem& setKisiSayisi( const int& kisiSayisi );
+    VillaItem& setHavuz( const std::string &havuz );
+    VillaItem& setFotoList( const QVector<bsoncxx::oid> list );
+    VillaItem& addFotoList( const bsoncxx::oid& fotoOid );
+    VillaItem& setYayinda( const bool& yayinda );
+
 
     const std::string adi() const;
     const std::string yeri() const;
     const std::string fotoOid() const;
     const std::string tanim() const;
+    int kisiSayisi() const;
+    const std::string havuz() const;
+    const bool yayinda() const;
+
+    const QVector<bsoncxx::oid> fotoList() const;
+
 
     void errorOccured(const std::string &errorText) override;
 };
