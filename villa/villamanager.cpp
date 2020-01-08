01050e53e88ba8509d2baf35a822d07c1198153e
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-08 19:51:40 +0300

    Kısmı Tamamlandı.

--- villa/villamanager.cpp
+++ villa/villamanager.cpp
@@ -1,6 +1,61 @@
 #include "villamanager.h"
-
-VillaManager::VillaManager()
+#include "eWidget/evillawidget.h"
+VillaManager::VillaManager(DB *_db , const bool& _adminPage)
+    :eCore::ListItem<VillaItem> (_db),
+      ContainerWidget ("Villalar"),
+      mAdminPage(_adminPage)
 {
+    setContentAlignment (AlignmentFlag::Center);
+    Content ()->setMaximumSize (1024,WLength::Auto);
+}
 
+void VillaManager::onList(const QVector<VillaItem> *mlist)
+{
+    if( this->mAdminPage )
+    {
+        this->initAdminList (mlist);
+    }else{
+        this->initPublicList (mlist);
+    }
+}
+
+void VillaManager::errorOccured(const std::string &errorText)
+{
+    this->showPopUpMessage (errorText,"hata");
+}
+
+void VillaManager::initAdminList( const QVector<VillaItem> *mlist )
+{
+    Content ()->clear ();
+    for( auto item : *mlist )
+    {
+        auto container = Content ()->addWidget (cpp14::make_unique<eWidget::eVillaWidget>(item));
+        container->addStyleClass (Bootstrap::Grid::Large::col_lg_3+
+                                  Bootstrap::Grid::Medium::col_md_3+
+                                  Bootstrap::Grid::Small::col_sm_4+
+                                  Bootstrap::Grid::ExtraSmall::col_xs_6);
+
+        container->ClickedVilla ().connect ([=]( const bsoncxx::oid& oid ){
+
+            std::cout << oid.to_string () << std::endl;
+        });
+    }
+}
+
+void VillaManager::initPublicList( const QVector<VillaItem> *mlist )
+{
+    Content ()->clear ();
+    for( auto item : *mlist )
+    {
+        auto container = Content ()->addWidget (cpp14::make_unique<eWidget::eVillaWidget>(item));
+        container->addStyleClass (Bootstrap::Grid::col_full_12);
+        container->addStyleClass (Bootstrap::Grid::Large::col_lg_3+
+                                  Bootstrap::Grid::Medium::col_md_3+
+                                  Bootstrap::Grid::Small::col_sm_4+
+                                  Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->ClickedVilla ().connect ([=]( const bsoncxx::oid& oid ){
+
+            std::cout << oid.to_string ()<< std::endl;
+        });
+    }
 }
--- villa/villamanager.h
+++ villa/villamanager.h
@@ -2,10 +2,28 @@
 #define VILLAMANAGER_H
 
 
-class VillaManager : public Container, public ListItem<VillaItem>
+#include "eCore/containerwiget.h"
+#include "eCore/listitem.h"
+#include "villa.h"
+#include "eWidget/evillawidget.h"
+
+class VillaManager : public ContainerWidget, public eCore::ListItem<VillaItem>
 {
 public:
-    VillaManager();
+    explicit VillaManager( eCore::DB* _db , const bool& _adminPage = false );
+
+    virtual void onList(const QVector<VillaItem> *mlist) final;
+
+    virtual void errorOccured(const std::string &errorText) final;
+
+
+private:
+
+    bool mAdminPage;
+
+    void initAdminList(const QVector<VillaItem>*);
+
+    void initPublicList(const QVector<VillaItem>*);
 };
 
 #endif // VILLAMANAGER_H
