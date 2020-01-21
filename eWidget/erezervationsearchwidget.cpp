67a1723dff9ba8e7d8f99dff9eb4d87e411dbf00
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-21 18:12:23 +0300

    Bazı Sınıflar Eklendi.

--- eWidget/erezervationsearchwidget.cpp
+++ eWidget/erezervationsearchwidget.cpp
@@ -1,6 +1,19 @@
 #include "erezervationsearchwidget.h"
 
-eRezervationSearchWidget::eRezervationSearchWidget()
+eWidget::eRezervationSearchWidget::eRezervationSearchWidget()
 {
+
+}
+
+
+eWidget::eRezervationCalenderWidget::eRezervationCalenderWidget( const std::string villaOid )
+{
+
+}
+
+eWidget::eRezervationMakeWidget::eRezervationMakeWidget(const std::string &villaOid)
+{
+
+
 
 }
--- eWidget/erezervationsearchwidget.h
+++ eWidget/erezervationsearchwidget.h
@@ -1,11 +1,29 @@
 #ifndef EREZERVATIONSEARCHWIDGET_H
 #define EREZERVATIONSEARCHWIDGET_H
-
+#include "eCore/containerwiget.h"
+namespace eWidget{
 
 class eRezervationSearchWidget : public ContainerWidget
 {
 public:
     eRezervationSearchWidget();
 };
+
+
+
+class eRezervationCalenderWidget : public ContainerWidget
+{
+public:
+    eRezervationCalenderWidget( const std::string villaOid );
+};
+
+
+class eRezervationMakeWidget : public ContainerWidget
+{
+public:
+    eRezervationMakeWidget( const std::string& villaOid );
+};
+
+}
 
 #endif // EREZERVATIONSEARCHWIDGET_H
